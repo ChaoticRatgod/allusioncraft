db6d33340f6a039135dd44faddfb030129a83e51
ChaoticRatgod <WrenReis@gmail.com>
2024-03-10 23:07:40 -0400

    Initial commit

---
+++ src/main/java/net/wren/allusioncraft/AllusionCraftDataGenerator.java
@@ -0,0 +1,11 @@
+package net.wren.allusioncraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AllusionCraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
